19bf0da078ae1f66cfaba926356f22ad19ba8842
Faisal <muhammadfaisalmalik01@gmail.com>
2025-05-22 23:37:27 +0530

    Add new components and fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
 
-import Layout  from './layout.jsx'
+
+import Layout  from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
 import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+import NotFound from './components/NotFound/NotFound.jsx';
+
 
 // const router = createBrowserRouter([
 //   {
@@ -39,11 +47,8 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
-      <Route 
-      loader={githubInfoLoader}
-      path='github' 
-      element={<Github />}
-       />
+      <Route path='github' loader={githubInfoLoader} element={<Github />} />
+      <Route path='*' element={<NotFound />} />
     </Route>
   )
 )
